6ffc46a8d36c0b98b86a18ee1572fbfad7ec9e26
Ibrahim Ibrahim <ibyster824@gmail.com>
2022-10-25 18:58:29 +1100

    Authentication added, not officially with django.

--- rooms/consumers.py
+++ rooms/consumers.py
@@ -1,10 +1,25 @@
+import environ
+import firebase_admin
 import json 
 
 from channels.generic.websocket import WebsocketConsumer
+from firebase_admin import credentials
+from pydantic import Json
 
 class RoomConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+
+    def connect(self, identifier):
+        # On connect verification will be performed, Via firebase-admin
+        cred = credentials.Certificate(environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+        verified =firebase_admin.initialize_app(cred)
+
+        id_token = Json.loads(identifier)
+        decoded_token = firebase_admin.verify_id_token(id_token)
+        uid = decoded_token["uid"]
+
+        if (uid != None):
+            self.accept()
+
 
     def disconnect(self, close_code):
         pass
