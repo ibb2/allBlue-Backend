40cb422aebff0cfc82272e984c2d63eabb7d5fe6
ibb2 <70052141+ibb2@users.noreply.github.com>
2022-11-07 13:11:21 +1100

    Django custom user model updates, should be passwordless now. SuperUser created

---
+++ users/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = (
+        "username",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "username",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email", "username")
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+# Local imports
+from .managers import CustomUserManager
 
 # Create your models here.
 class User(AbstractUser):
-    pass
 
+    manager = CustomUserManager()
